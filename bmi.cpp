d9ca8847f44833a28cf90d8d19ab51dc4ed958f3
jyppro <87540231+jyppro@users.noreply.github.com>
2022-08-23 08:48:58 +0900

    Add files via upload

---
+++ bmi.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include <stdio.h>
+
+int main()
+{
+	double height;
+	double weight;
+	double bmi;
+
+	printf("********* BMI(체질량 지수) ***********\n\n");
+
+	printf("체중(kg) 입력:");
+	scanf_s("%lf", &weight);
+	printf("키(m^2) 입력:");
+	scanf_s("%lf", &height);
+
+	height = height / 100;
+	bmi = weight / (height * height);
+
+	if (bmi < 18.6)
+		printf("당신의 BMI는%.1f이므로, 저체중 입니다.\n", bmi);
+	else if (bmi < 23)
+		printf("당신의 BMI는%.1f이므로, 정상 입니다.\n", bmi);
+	else if (bmi < 25)
+		printf("당신의 BMI는%.1f이므로, 과체중 입니다.\n",bmi);
+	else if (bmi < 30)
+		printf("당신의 BMI는%.1f이므로, 비만 입니다.\n",bmi);
+	else if (bmi < 40)
+		printf("당신의 BMI는%.1f이므로, 고도비만 입니다.\n",bmi);
+	
+
+	return 0;
+}
