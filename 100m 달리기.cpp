ab871f0b581b2b16ac75d8bbfcc9f8e031266c9f
jyppro <87540231+jyppro@users.noreply.github.com>
2022-08-22 10:17:55 +0900

    Add files via upload
    1st grade school tutoring class

---
+++ 100m 달리기.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include <stdio.h>
+
+int main()
+{
+	float pos = 0.0f; /*거리*/
+	float speed = 1.0f; /*속도*/
+	
+	while (pos <= 100)
+	{	pos += speed;
+	 printf("현재 위치는 %.2fm입니다.\n", pos);
+	 { if (pos <= 50) 
+		 speed += 1.0f;
+	   if (pos > 50)
+		 speed -= 0.5f;
+	 }
+	}
+
+	printf("최종 거리는 %.2fm입니다.\n", pos);
+	return 0;
+}
