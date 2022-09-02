21f03d6b52a4b70a35eeb7d2db838f0cc3740987
jyppro <87540231+jyppro@users.noreply.github.com>
2022-09-03 07:36:37 +0900

    Add files via upload
    Grade 2 Start

---
+++ 1학기 알고리즘 1주차.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main() {
+	int i, answer, score = 0, num, correct;
+	for (i = 0; i < 5; i++) {
+		correct = 0;
+		num = 0;
+		while (num < 3 && !correct) {
+			printf("%d + %d = ", i*i, i * 10);
+			scanf("%d", &answer);
+			if (answer == (i*i) + (i * 10)) {
+				printf("Correct!!\n");
+				correct = 1;
+				score += (20 - num * 3);
+			}
+			else {
+				if (num != 2) printf("Try Again!!\n");
+				else printf("Answer = %d\n", (i*i) + (i * 10));
+			}
+			num++;
+		}
+	}
+	printf("덧셈성적은 %d\n", score);
+}
