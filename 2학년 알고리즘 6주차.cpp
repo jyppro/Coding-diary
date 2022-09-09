674319050131d73399d84121499f694f59e8b61f
jyppro <87540231+jyppro@users.noreply.github.com>
2022-09-09 21:11:26 +0900

    Add files via upload

---
+++ 2학년 알고리즘 6주차.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+int bsearch(int a[], int n, int key) { // 오름차순
+	int mid;
+	int left = 0, right = n - 1;
+	while (left <= right) {
+		mid = (left + right) / 2;
+		if (key < a[mid]) mid = right - 1;
+		else if (key > a[mid]) mid = left + 1;
+		else return mid;
+	}
+	return -1;
+}
