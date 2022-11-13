577036265bf66cd5d3ea4b29ffa51252708f75ba
jyppro <87540231+jyppro@users.noreply.github.com>
2022-11-13 11:26:31 +0900

    Add files via upload

---
+++ Exam09.java
@@ -0,0 +1,12 @@
+package week04;
+
+public class Exam09 {
+
+	public static void main(String[] args) {
+		Automobile auto = new Automobile();
+		
+		auto.upSpeed(250);
+		System.out.println("승용차의 속도는" + auto.getSpeed() + "km입니다. ");
+	}
+
+}
