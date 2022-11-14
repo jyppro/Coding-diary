2fd20512aa2881b5e0a982884d2b4529f2fbfa3a
jyppro <87540231+jyppro@users.noreply.github.com>
2022-11-14 11:03:54 +0900

    Add files via upload

---
+++ InheritanceEx.java
@@ -0,0 +1,11 @@
+package week04;
+
+public class InheritanceEx {
+
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.set();
+		System.out.println("s°´Ã¼ÀÇ °ª" + s.name + " " + s.age + " " + s.height + " " + s.getWeight());
+	}
+
+}
