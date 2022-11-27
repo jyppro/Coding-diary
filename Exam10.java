f09f3ea90e7839b6460bf74227b8a3eb39d59efb
jyppro <87540231+jyppro@users.noreply.github.com>
2022-11-27 17:54:24 +0900

    Add files via upload

---
+++ Exam10.java
@@ -0,0 +1,14 @@
+package week05;
+
+public class Exam10 {
+
+	public static void main(String[] args) {
+		Tiger tiger1 = new Tiger();
+		Eagle eagle1 = new Eagle();
+		
+		tiger1.move();
+		eagle1.move();
+
+	}
+
+}
