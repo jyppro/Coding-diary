ec55ef2ac76a5c8e9115c0b0c38903a6fd8265e4
jyppro <87540231+jyppro@users.noreply.github.com>
2022-11-29 20:19:30 +0900

    Add files via upload

---
+++ Exam12.java
@@ -0,0 +1,13 @@
+package week05;
+
+public class Exam12 {
+
+	public static void main(String[] args) {
+		iCat cat = new iCat();
+		cat.eat();
+		
+		iTiger tiger = new iTiger();
+		tiger.move();
+		tiger.eat();
+	}
+}
