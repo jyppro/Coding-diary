d1e696e07ed3a91720dc4bd28cd6f44dd4a2fc29
jyppro <87540231+jyppro@users.noreply.github.com>
2023-01-12 21:32:00 +0900

    Add files via upload

---
+++ Ex11_14.java
@@ -0,0 +1,29 @@
+package week13;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Ex11_14 extends JFrame{
+	public Ex11_14() {
+		setTitle("슬라이더 만들기 예제 20190816 박재영");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 200, 100);
+		slider.setPaintLabels(true);
+		slider.setPaintTicks(true);
+		slider.setPaintTrack(true);
+		slider.setMajorTickSpacing(50);
+		slider.setMinorTickSpacing(10);
+		
+		c.add(slider);
+		setSize(300, 100);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Ex11_14();
+	}
+
+}
