8e8de35d5a60541033db00a269921554041dc2fd
jyppro <87540231+jyppro@users.noreply.github.com>
2023-01-25 15:19:24 +0900

    Add files via upload

---
+++ Define1.java
@@ -0,0 +1,9 @@
+public class Define1 {
+	//상수정의
+	public static final int SW = 1001;
+	public static final int INFO = 1002;
+	public static final int TONGSIN = 2001;
+	public static final int DESIGN = 3001;
+	public static final int KY = 4001;
+	public static final int AI = 1003;
+}
