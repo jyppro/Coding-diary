366fc28a6d74cebf2655bf3d134d6f4a554863c3
jyppro <87540231+jyppro@users.noreply.github.com>
2023-01-26 14:59:55 +0900

    Add files via upload

---
+++ Define2.java
@@ -0,0 +1,7 @@
+public class Define2 {
+	public static final int sw = 1001;
+	public static final int info = 1002;
+	public static final int tongsin = 1003;
+	public static final int ky = 1004;
+	public static final int ai = 1005;
+}
