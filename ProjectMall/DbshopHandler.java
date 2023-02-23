012597993a89b42beff4d9404ff9fe004caee3b4
jyppro <87540231+jyppro@users.noreply.github.com>
2023-02-24 00:56:36 +0900

    Add files via upload

---
+++ ProjectMall/DbshopHandler.java
@@ -0,0 +1,11 @@
+package com.dbshop;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface DbshopHandler {
+	public String process(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException;
+}
