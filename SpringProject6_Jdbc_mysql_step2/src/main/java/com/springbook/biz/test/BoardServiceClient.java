c18e25a4b05986ab9458929f72254a4372155f28
jyppro <87540231+jyppro@users.noreply.github.com>
2023-06-08 17:44:39 +0900

    Add files via upload

---
+++ SpringProject6_Jdbc_mysql_step2/src/main/java/com/springbook/biz/test/BoardServiceClient.java
@@ -0,0 +1,33 @@
+﻿package com.springbook.biz.test;
+
+import java.util.List;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.springbook.biz.board.BoardService;
+import com.springbook.biz.board.BoardVO;
+
+public class BoardServiceClient {
+	public static void main(String[] args) {
+		AbstractApplicationContext container = 
+				new GenericXmlApplicationContext("applicationContext.xml");
+
+		BoardService boardService = (BoardService) container.getBean("boardService");
+
+		BoardVO vo = new BoardVO();
+		//vo.setSeq(100);
+		vo.setTitle("9월25일");
+		vo.setWriter("d장용미(본인이름)");
+		vo.setContent("임시 내용..............");
+		boardService.insertBoard(vo);
+
+		List<BoardVO> boardList = boardService.getBoardList(vo);
+		for (BoardVO board : boardList) {
+			System.out.println("---> " + board.toString());
+		}
+
+		// 5. Spring 컨테이너 종료
+		container.close();
+	}
+}
