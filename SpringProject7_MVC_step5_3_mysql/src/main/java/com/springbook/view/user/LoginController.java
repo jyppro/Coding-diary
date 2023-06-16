a84a5ee05c58f3f095357b905b887b4a10fb195d
jyppro <87540231+jyppro@users.noreply.github.com>
2023-06-16 23:57:05 +0900

    Add files via upload

---
+++ SpringProject7_MVC_step5_3_mysql/src/main/java/com/springbook/view/user/LoginController.java
@@ -0,0 +1,32 @@
+package com.springbook.view.user;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.springbook.biz.user.UserVO;
+import com.springbook.biz.user.impl.UserDAO;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping(value = "/login.do", method = RequestMethod.GET)
+	public String loginView(@ModelAttribute("user") UserVO vo) {
+		vo.setId("test");
+		vo.setPassword("test123");
+		return "login.jsp";
+	}
+
+	@RequestMapping(value = "/login.do", method = RequestMethod.POST)
+	public String login(UserVO vo, UserDAO userDAO, HttpSession session) {
+		UserVO user = userDAO.getUser(vo);
+		if (user != null) {
+			session.setAttribute("userName", user.getName());
+			return "getBoardList.do";
+		} else
+			return "login.jsp";
+	}
+}
