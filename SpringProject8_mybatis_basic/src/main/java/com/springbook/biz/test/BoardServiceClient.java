5b7d2beea820c87a6d0d81c0405c072befcc429a
jyppro <87540231+jyppro@users.noreply.github.com>
2023-06-20 14:24:56 +0900

    Add files via upload

---
+++ SpringProject8_mybatis_basic/src/main/java/com/springbook/biz/test/BoardServiceClient.java
@@ -0,0 +1,29 @@
+﻿package com.springbook.biz.test;
+
+
+import java.util.List;
+
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.board.impl.BoardDAO;
+
+
+
+public class BoardServiceClient {
+	public static void main(String[] args) {
+		
+		BoardDAO boardDAO = new BoardDAO();
+		BoardVO vo = new BoardVO();
+		vo.setTitle("my11111");
+		vo.setWriter("홍길동");
+		vo.setContent("myBatis 내용입니다.....");
+		boardDAO.insertBoard(vo);
+		
+
+		List<BoardVO> boardList = boardDAO.getBoardList(vo);
+		for (BoardVO board : boardList) {
+			System.out.println("---> " + board.toString());
+		}		
+		
+
+	}
+}
