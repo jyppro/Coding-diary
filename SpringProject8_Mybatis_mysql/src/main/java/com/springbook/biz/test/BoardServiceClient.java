ea3b127e84b537a1d10f4b8d8a53d98f6f872dc6
jyppro <87540231+jyppro@users.noreply.github.com>
2023-06-21 09:50:28 +0900

    Add files via upload

---
+++ SpringProject8_Mybatis_mysql/src/main/java/com/springbook/biz/test/BoardServiceClient.java
@@ -0,0 +1,36 @@
+﻿package com.springbook.biz.test;
+
+
+import java.util.List;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.springbook.biz.board.BoardService;
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.board.impl.BoardDAOMybatis;
+
+
+
+public class BoardServiceClient {
+	public static void main(String[] args) {
+		AbstractApplicationContext container = new GenericXmlApplicationContext("applicationContext.xml");
+
+		// 2. Spring 컨테이너로부터 BoardServiceImpl 객체를 Lookup한다.
+		BoardService boardService = (BoardService) container.getBean("boardService");
+		
+		BoardVO vo = new BoardVO();
+		vo.setTitle("my11111");
+		vo.setWriter("홍길동");
+		vo.setContent("myBatis 내용입니다.....");
+		boardService.insertBoard(vo);
+		
+
+		List<BoardVO> boardList = boardService.getBoardList(vo);
+		for (BoardVO board : boardList) {
+			System.out.println("---> " + board.toString());
+		}		
+		container.close();
+
+	}
+}
